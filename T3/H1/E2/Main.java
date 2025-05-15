3325be3e0a1fdd00f46d1bdee94e8a30fd053625
JLarry00 <juanlarrondofdez@gmail.com>
2025-05-15 22:11:49 +0200

    nada

--- T3/H1/E2/Main.java
+++ T3/H1/E2/Main.java
@@ -14,6 +14,11 @@ public class Main {
         RAMHP hpRAM = FactoryHP.createRAM(8, 2400);
         PlacaBaseHP hpPlacaBase = FactoryHP.createPB("AMD", "Radeon", 6, 3);
 
+        // Create X components
+        HDX xHD = FactoryX.createHD(256, "HDD");
+        RAMX xRAM = FactoryX.createRAM(4, 2400);
+        PlacaBaseX xPlacaBase = FactoryX.createPB("Intel", "NVIDIA", 4, 2);
+
         // Create a DELL computer with DELL components
         OrdenadorDELL dellOrdenador = FactoryDELL.createOrdenador("DELL", dellHD, dellRAM, dellPlacaBase);
         System.out.println("Created a DELL computer with DELL components: " + dellOrdenador);
@@ -21,5 +26,9 @@ public class Main {
         // Create an HP computer with HP components
         OrdenadorHP hpOrdenador = FactoryHP.createOrdenador("HP", hpHD, hpRAM, hpPlacaBase);
         System.out.println("Created an HP computer with HP components: " + hpOrdenador);
+        
+        // Create a X computer with X components
+        OrdenadorX xOrdenador = FactoryX.createOrdenador("X", xHD, xRAM, xPlacaBase);
+        System.out.println("Created a X computer with X components: " + xOrdenador);
     }
 }
