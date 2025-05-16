587e239980fcb630662ececcfecd0e6aa666ca20
JLarry00 <juanlarrondofdez@gmail.com>
2025-05-16 19:29:01 +0200

    nada

--- T3/H1/E4/Proxy/CuentaProxy.java
+++ T3/H1/E4/Proxy/CuentaProxy.java
@@ -5,28 +5,28 @@ import java.io.*;
 public class CuentaProxy implements ICuenta {
     private Cuenta cuenta;
     private String pwd;
-    private boolean acceso;
+    private boolean correcta;
 
     public CuentaProxy(int disponible, String pwd) {
         this.cuenta = new Cuenta(disponible);
         this.pwd = pwd;
-        this.acceso = false;
+        this.correcta = false;
     }
 
     @Override
     public int obtenerDisponible() {
-        if (acceso == false) {
+        if (correcta == false) {
             System.out.print("Ingrese la contraseña: ");
             BufferedReader scanner = new BufferedReader(new InputStreamReader(System.in));
             try {
                 String inputPwd = scanner.readLine();
-                if (inputPwd.equals(this.pwd)) acceso = true;
+                if (inputPwd.equals(this.pwd)) correcta = true;
                 else System.out.println("Contraseña incorrecta.");
             } catch (IOException e) { return -1; }
         }
-        if (acceso == true) return cuenta.obtenerDisponible();
+        if (correcta == true) return cuenta.obtenerDisponible();
         return -1;
     }
 
-    public void logout() { acceso = false; }
+    public void logout() { correcta = false; }
 }
